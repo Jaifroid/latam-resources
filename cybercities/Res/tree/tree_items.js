17ae9655a742c6c656f73d0fcfbdfe38cb4d8708
Jaifroid <egk10@cam.ac.uk>
2024-01-30 17:07:33 +0000

    Add initial files

---
+++ cybercities/Res/tree/tree_items.js
@@ -0,0 +1,11 @@
+var TREE_ITEMS = [
+	['Home','index.html',{'sb':' '},
+		['1. Introduction', 'Introduction.html',{'tt':'1. Introduction','sb':'Introduction.html'},],
+		['2. Argentine tradition', 'Argentinetradition.html',{'tt':'2. Argentine tradition','sb':'Argentinetradition.html'},],
+		['3. Dystopian cities', 'Dystopiancities.html',{'tt':'3. Dystopian cities','sb':'Dystopiancities.html'},],
+		['4. Paradoxical (urban) topolog... ', 'Paradoxical(urban)to.html',{'tt':'4. Paradoxical (urban) topologies','sb':'Paradoxical(urban)to.html'},],
+		['5. Cyborg woman / story-tellin... ', 'Cyborgwoman.story-te.html',{'tt':'5. Cyborg woman / story-telling machine','sb':'Cyborgwoman.story-te.html'},],
+		['6. Conclusion', 'Conclusion.html',{'tt':'6. Conclusion','sb':'Conclusion.html'},],
+
+]
+];
